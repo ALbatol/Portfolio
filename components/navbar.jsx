603cad9365862470d57fa953bf8e216023c9cffe
alb.aldossary <alb.aldossary@tamkeentech.sa>
2024-04-22 14:19:18 +0300

    fixing navber

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = ()=> {
 className=" inline-block h-[220px] mt-16 min-h-[1em] w-0.5 self-stretch bg-primary dark:bg-white/10"></div>
 </div>
 {/* nav for small screans  */}
-<div className="px-[4%] pt-20 float-right md:hidden flex flex-col items-end" >
+<div className="px-[4%] py-10 float-right md:hidden flex flex-col items-end" >
   <div className=" " onClick={()=>setopenSideNav((prev) => !prev)}>
 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
     <path stroke="#96031A" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
